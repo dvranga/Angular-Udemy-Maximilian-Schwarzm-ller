8a6de785bd13bca7aff5b6d81ed88d9c5e8b6fab
dvranga <dvranga@users.noreply.github.com>
2021-02-13 08:38:16 +0530

    Assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -18,6 +18,13 @@ export class ServersComponent implements OnInit {
   userName='';
   serverCreated=false;
   servers=['TestServer', 'TestServer 2'];
+  details=['Secret password is = Tuna'];
+  show=false;
+  count=0;
+  color='red';
+  showSecrect=false;
+
+  log=[];
 
   serverCreationStatus='No server was created !';
 
@@ -45,5 +52,30 @@ export class ServersComponent implements OnInit {
 
   Reset(){
     return this.userName='';
+  }
+
+  getDisplayDetails(){
+    this.show=true;
+    this.details.push(this.serverName);
+    this.count=this.count+1
+  }
+
+  getColor(){
+    if (this.count>5) {
+      return 'green';
+    }
+  }
+
+  getCSSColor(){
+    if (this.count>5) {
+      console.log(this.count)
+      return 'white';
+    }
+  }
+
+  onToggleDetails(){
+    this.showSecrect=!this.showSecrect;
+    // this.log.push(this.log.length+1);
+    this.log.push(new Date());
   }
 }
